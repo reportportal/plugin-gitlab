a3aae2894aa6bc40db71770da711b0531e7ec65d
Andrei Piankouski <andrei_piankouski@epam.com>
2023-11-08 13:34:57 +0300

    EPMRPP-87411 || All spaces are replaced with %20 if post issue to GitLab

--- src/main/java/com/epam/reportportal/extension/gitlab/rest/client/GitlabClient.java
+++ src/main/java/com/epam/reportportal/extension/gitlab/rest/client/GitlabClient.java
@@ -102,9 +102,7 @@ public class GitlabClient {
     private String getUrl(String url, Map<String, List<String>> queryParams) {
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(url);
         queryParams.keySet().forEach(key -> uriComponentsBuilder.queryParam(key, queryParams.get(key)));
-        return uriComponentsBuilder
-                .encode()
-                .toUriString();
+        return uriComponentsBuilder.build(false).toString();
 
     }
 
