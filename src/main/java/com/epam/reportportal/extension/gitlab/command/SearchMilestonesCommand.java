f3a17b988c4bc77834af359da07069798b876490
Pavel_Bortnik <pavel_bortnik@epam.com>
2023-11-10 10:21:52 +0300

    EPMRPP-87316 || Add headers

--- src/main/java/com/epam/reportportal/extension/gitlab/command/SearchMilestonesCommand.java
+++ src/main/java/com/epam/reportportal/extension/gitlab/command/SearchMilestonesCommand.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.epam.reportportal.extension.gitlab.command;
 
 import static java.util.Optional.ofNullable;
@@ -13,6 +28,9 @@ import com.epam.ta.reportportal.ws.model.ErrorType;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
+ */
 public class SearchMilestonesCommand implements PluginCommand<List<MilestoneDto>> {
 
   private final GitlabClientProvider gitlabClientProvider;
