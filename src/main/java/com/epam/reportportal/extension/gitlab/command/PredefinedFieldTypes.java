9153561f3a2850168d5d25babeae7a658bcd64bb
Pavel_Bortnik <pavel_bortnik@epam.com>
2023-11-23 18:16:27 +0300

    EPMRPP-87336 || Fix collection of named value

--- src/main/java/com/epam/reportportal/extension/gitlab/command/PredefinedFieldTypes.java
+++ src/main/java/com/epam/reportportal/extension/gitlab/command/PredefinedFieldTypes.java
@@ -8,7 +8,7 @@ public abstract class PredefinedFieldTypes {
   public static final String MULTI_AUTOCOMPLETE = "multiAutocomplete";
   public static final String CREATABLE_MULTI_AUTOCOMPLETE = "creatableMultiAutocomplete";
 
-  public static final List<String> NAMED_VALUE_FIELDS = List.of(MULTI_AUTOCOMPLETE,
+  public static final List<String> NAMED_VALUE_FIELDS = List.of(AUTOCOMPLETE, MULTI_AUTOCOMPLETE,
       CREATABLE_MULTI_AUTOCOMPLETE);
 
 
